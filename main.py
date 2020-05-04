194d41af4ae98c40975ce5a3ad0e66ad50bf6332
Josh Long <josh@joshlong.com>
2020-05-03 22:32:23 -0700

    up

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
 import os
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/hi')
+def hello():
+    return {'message': "Hello World!"}
 
 if __name__ == '__main__':
     fp = os.path.join(os.environ['HOME'], 'Desktop', 'hello.txt')
@@ -7,3 +15,5 @@ if __name__ == '__main__':
         os.makedirs(fp)
     with open(fp, 'w') as f:
         f.write('Nihao!')
+
+    app.run(port=8080)
