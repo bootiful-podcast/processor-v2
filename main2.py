7b6c204b670709acb6e870d27efcf2cca9e3374e
Josh Long <josh@joshlong.com>
2020-05-04 03:36:10 -0700

    deploy

--- main2.py
+++ main2.py
@@ -1,13 +1,13 @@
 import os
 
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route("/")
-def hello():
-    return "Hello World!"
+# from flask import Flask
+#
+# app = Flask(__name__)
+#
+#
+# @app.route("/")
+# def hello():
+#     return "Hello World!"
 
 
 if __name__ == "__main__":
