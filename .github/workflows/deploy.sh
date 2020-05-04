a9889fcef5e62d5d364f44b47d9e4dced8bb1775
Josh Long <josh@joshlong.com>
2020-05-04 01:49:55 -0700

    up

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -7,6 +7,5 @@
 USER_DATA_URL=https://raw.githubusercontent.com/bootiful-podcast/python-test-to-deploy/${GITHUB_SHA}/.github/workflows/bootstrap.sh
 echo $USER_DATA_URL
 USER_DATA=$(curl $USER_DATA_URL )
-echo "user data:"
-echo "${USER_DATA}"
+echo "user data: ${USER_DATA}"
 aws ec2 run-instances --region $AWS_REGION --image-id $AMI_ID   --count 1 --instance-type $INSTANCE_TYPE --key-name $KEYPAIR --security-groups $SECURITY_GROUP --user-data "$USER_DATA"
