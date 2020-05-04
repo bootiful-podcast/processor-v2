49bfb623ce8b716305bf782fc94deee96f15c723
Josh Long <josh@joshlong.com>
2020-05-04 05:47:44 -0700

    up

--- .github/workflows/deploy-local.sh
+++ .github/workflows/deploy-local.sh
@@ -8,7 +8,6 @@ INSTANCE_TYPE=t3.large
 SUBNET_ID=subnet-05ec18a303fb18a5c
 USER_DATA_URL=https://raw.githubusercontent.com/bootiful-podcast/python-test-to-deploy/master/.github/workflows/bootstrap.sh
 USER_DATA=$(curl $USER_DATA_URL )
-#aws ec2 run-instances --region $AWS_REGION --image-id $AMI_ID --count 1 --instance-type $INSTANCE_TYPE --key-name $KEYPAIR --security-groups $SECURITY_GROUP --user-data "$USER_DATA" --subnet $SUBNET_ID
 aws ec2 run-instances --region $AWS_REGION --image-id $AMI_ID --count 1 --instance-type $INSTANCE_TYPE --key-name $KEYPAIR   --user-data "$USER_DATA" --subnet $SUBNET_ID
 
 
--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
-#KEYPAIR=bootiful-podcast_processor_05-02-2020
+
+
+KEYPAIR=udemy-aws-oregon
 #SECURITY_GROUP=web-host
-#AMI_ID=ami-06fcc1f0bc2c8943f
-#REGION=us-west-1
-#INSTANCE_TYPE=t1.micro
-USER_DATA_URL=https://raw.githubusercontent.com/bootiful-podcast/python-test-to-deploy/${GITHUB_SHA}/.github/workflows/bootstrap.sh
+SECURITY_GROUP=launch-wizard-1
+AMI_ID=ami-0d6621c01e8c2de2c
+AWS_REGION=us-west-2
+INSTANCE_TYPE=t3.large
+SUBNET_ID=subnet-05ec18a303fb18a5c
+USER_DATA_URL=https://raw.githubusercontent.com/bootiful-podcast/python-test-to-deploy/master/.github/workflows/bootstrap.sh
 USER_DATA=$(curl $USER_DATA_URL )
-aws ec2 run-instances --region $AWS_REGION --image-id $AMI_ID --count 1 --instance-type $INSTANCE_TYPE --key-name $KEYPAIR --security-groups $SECURITY_GROUP --user-data "$USER_DATA"
+aws ec2 run-instances --region $AWS_REGION --image-id $AMI_ID --count 1 --instance-type $INSTANCE_TYPE --key-name $KEYPAIR   --user-data "$USER_DATA" --subnet $SUBNET_ID
+
 
