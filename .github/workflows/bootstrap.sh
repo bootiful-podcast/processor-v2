e2f204bb493c597a47523794edf54b953ea82349
Josh Long <josh@joshlong.com>
2020-05-04 02:48:36 -0700

    bootstrap

--- .github/workflows/bootstrap.sh
+++ .github/workflows/bootstrap.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-EC2_HOME=/home/ec2-user
-APP_HOME=$EC2_HOME/app
-LOG=$EC2_HOME/test.log
-mkdir -p $APP_HOME
-yum install -y python37 python37-pip git
-mkdir -p $EC2_HOME
-users  >> $EC2_HOME/users.txt
-echo "$HOME $(whoami)" >> $LOG
-su ec2-user 
-echo "$HOME $(whoami) " >> $LOG
-pip3 install pipenv supervisor
-pipenv >> $LOG
-git clone https://github.com/bootiful-podcast/python-test-to-deploy.git $APP_HOME
+function install() {
+  EC2_HOME=/home/ec2-user
+  APP_HOME=$EC2_HOME/app
+  LOG=$EC2_HOME/test.log
+  mkdir -p $APP_HOME
+  yum install -y python37 python37-pip git
+  mkdir -p $EC2_HOME
+  users
+  su ec2-user
+  pip3 install pipenv supervisor
+  git clone https://github.com/bootiful-podcast/python-test-to-deploy.git $APP_HOME
+  cd $APP_HOME
+  pipenv run python3 main2.py
+}
+
+install >> $LOG
 
 ### supervisord
 #cd $APP_HOME && pipenv install # && pipenv run python  main2.py
