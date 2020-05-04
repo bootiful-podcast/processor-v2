481801156ac8ef5ba1022565485d1a8c210f3ade
Josh Long <josh@joshlong.com>
2020-05-04 00:59:24 -0700

    deploy

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-KEYPAIR=bootiful-podcast_processor_05-02-2020
-SECURITY_GROUP=web-host
-AMI_ID=ami-06fcc1f0bc2c8943f
-REGION=us-west-1
-INSTANCE_TYPE=t1.micro
-USER_DATA=$(curl https://raw.githubusercontent.com/bootiful-podcast/python-test-to-deploy/master/.github/workflows/bootstrap.sh)
+#KEYPAIR=bootiful-podcast_processor_05-02-2020
+#SECURITY_GROUP=web-host
+#AMI_ID=ami-06fcc1f0bc2c8943f
+#REGION=us-west-1
+#INSTANCE_TYPE=t1.micro
+USER_DATA=$(curl https://raw.githubusercontent.com/bootiful-podcast/python-test-to-deploy/${GITHUB_SHA}/.github/workflows/bootstrap.sh )
 aws ec2 run-instances --image-id $AMI_ID --region $REGION --count 1 --instance-type $INSTANCE_TYPE --key-name $KEYPAIR --security-groups $SECURITY_GROUP --user-data "$USER_DATA"
