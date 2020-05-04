5ab76fc80f69433e8017d7867032acb9ea23ae14
Josh Long <josh@joshlong.com>
2020-05-04 03:10:56 -0700

    up

--- main2.py
+++ main2.py
@@ -1,8 +1,21 @@
 import os
 
-if __name__ == '__main__':
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+if __name__ == "__main__":
+    app.run()
+
     print('hello, world!')
     home = os.environ['HOME']
     path = os.path.join(home, 'hello.txt')
     with open(path, 'w') as fp:
         fp.write('hello, world!')
+
