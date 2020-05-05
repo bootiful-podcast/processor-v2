ac6ea3b1cbf1e3e21e23f76d394c93392546f6be
Josh Long <josh@joshlong.com>
2020-05-05 15:44:57 -0700

    deploy

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -61,8 +61,8 @@ if [ "$(aws ec2 describe-key-pairs --region $AWS_REGION | jq -r '.KeyPairs[].Key
 fi
 
 ## Run the instance on EC2
-IMAGE_NAME=$(aws ec2 describe-images --region $AWS_REGION --owners amazon --filters 'Name=name,Values=amzn-ami-hvm-????.??.?.x86_64-gp2' 'Name=state,Values=available' | jq -r '.Images | sort_by(.CreationDate) | last(.[]).ImageId')
-IMAGE_NAME=$AMI_ID #todo can we fix this later?
+#IMAGE_NAME=$(aws ec2 describe-images --region $AWS_REGION --owners amazon --filters 'Name=name,Values=amzn-ami-hvm-????.??.?.x86_64-gp2' 'Name=state,Values=available' | jq -r '.Images | sort_by(.CreationDate) | last(.[]).ImageId')
+IMAGE_NAME=$AMI_ID #todo can we fix this later? it'd be nice to query for the image and get the latest and greatest, i guess.
 INSTANCE_ID=$(aws ec2 run-instances --user-data "$USER_DATA" --region $AWS_REGION --image-id $IMAGE_NAME --count 1 --instance-type $INSTANCE_TYPE --key-name $KEYPAIR_NAME --security-group-ids $SG_ID --subnet-id $SUBNET_ID --associate-public-ip-address | jq -r '.Instances[0].InstanceId')
 echo "INSTANCE_ID=$INSTANCE_ID"
 DNS_NAME=$(aws ec2 describe-instances --region $AWS_REGION --instance-ids $INSTANCE_ID | jq -r  '.Reservations[0].Instances[0].PublicDnsName')
