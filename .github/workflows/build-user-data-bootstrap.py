62db5c9273bbde3d75bfc98ed8a6d720027425f8
Josh Long <josh@joshlong.com>
2020-05-06 03:47:15 -0700

    deploy

--- .github/workflows/build-user-data-bootstrap.py
+++ .github/workflows/build-user-data-bootstrap.py
@@ -15,5 +15,5 @@ if __name__ == '__main__':
     fragment = 'PODCAST_RMQ_ADDRESS=_PODCAST_RMQ_ADDRESS_'
     contents = urllib.request.urlopen(url_for_bootstrap_sh).read().decode("utf8")
     a, b = contents.split(fragment)
-    lines = [a, 'PODCAST_RMQ_ADDRESS=%s' % rmq_address, b]
+    lines = [a, 'PODCAST_RMQ_ADDRESS="%s"' % rmq_address, b]
     print(os.linesep.join(lines))
