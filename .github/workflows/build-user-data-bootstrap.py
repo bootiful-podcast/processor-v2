987bf43eef3fce9bac364500dc7ff4c24262ea84
Josh Long <josh@joshlong.com>
2020-05-06 02:51:40 -0700

    bootstrap

--- .github/workflows/build-user-data-bootstrap.py
+++ .github/workflows/build-user-data-bootstrap.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     url_for_bootstrap_sh = sys.argv[1]
     rmq_address = sys.argv[2]
     fragment = '_PODCAST_RMQ_ADDRESS_'
-    contents = urllib.request.urlopen(url_for_bootstrap_sh).read()
+    contents = str(urllib.request.urlopen(url_for_bootstrap_sh).read())
     print(contents)
     # os.linesep.join([str(x) for x in response.readlines()])
     # with urllib.request.urlopen(url_for_bootstrap_sh).read() as response:
