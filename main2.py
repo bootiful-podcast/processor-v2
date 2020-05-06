97d7ab8cdd0b2b13a4efbdec31fff4e4074c2dc0
Josh Long <josh@joshlong.com>
2020-05-05 19:18:55 -0700

    bootstrap

--- main2.py
+++ main2.py
@@ -6,9 +6,8 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return "Hello World!"
-
+def greet():
+    return {'greeting': 'Hello World!'}
 
 if __name__ == "__main__":
     print('hello, world!')
