2dac401b0f1e2ef80210236f44fb40e9600b4c2f
Josh Long <josh@joshlong.com>
2020-05-05 18:40:26 -0700

    deploy

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 whoami > "$HOME"/run.log
 
 pip3 install --user --upgrade pipenv
-start_dir=$(cd . && pwd)
+start_dir=$(cd `dirname $0` && pwd )
 cd $start_dir
 pipenv install && pipenv run python3 main2.py
