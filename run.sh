d6b39498820ed8c6235b85e120e23bd4857ce1a1
Josh Long <josh@joshlong.com>
2020-05-05 19:16:56 -0700

    bootstrap

--- run.sh
+++ run.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-LOG=/home/ec2-user/out.txt
-pwd >>$LOG
-start_dir=$(cd "$(dirname $0)" && pwd)
-cd $start_dir
-echo $start_dir >>$LOG
-pwd >>$LOG
-pip3 install --user --upgrade pipenv >>$LOG
+do_run() {
+  echo $HOME
+  whoami
+  pwd
+  start_dir=$(cd "$(dirname $0)" && pwd)
+  cd $start_dir
+  echo "$start_dir"
+  pwd
+  pip3 install --user --upgrade pipenv
+  PATH=$PATH:/home/ec2-user/.local/bin/
+  pipenv install
+  pipenv run python3 main2.py
+}
 
-PIPENV_PATH=/home/ec2-user/.local/bin/
-PATH=$PATH:$PIPENV_PATH
-
-pipenv install >>$LOG
-pipenv run python3 main2.py >>$LOG
+do_run >/home/ec2-user/run.log 2>&1
