d4452b7e974cdc3bf42d8484cee0092736876d2e
Josh Long <josh@joshlong.com>
2020-05-05 18:57:16 -0700

    up

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+LOG=/home/ec2-user/out.txt
+pwd >> $LOG
 start_dir=$(cd "$(dirname $0)" && pwd)
 cd $start_dir
+echo $start_dir >> $LOG
+pwd >> $LOG
 pip3 install --user --upgrade pipenv
 pipenv install && pipenv run python3 main2.py
