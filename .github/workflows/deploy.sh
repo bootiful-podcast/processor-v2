22298c75f31134094ceab5420f87193fde0870b8
Josh Long <josh@joshlong.com>
2020-05-09 01:20:22 -0700

    polish..

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -26,6 +26,7 @@ done
 
 #
 echo " $GITHUB_SHA : $PODCAST_RMQ_ADDRESS : $BP_MODE "
+echo "  $PODCAST_RMQ_ADDRESS_development :: $PODCAST_RMQ_ADDRESS_production   "
 KEYPAIR_FILE=$HOME/${KEYPAIR_NAME}.pem
 USER_DATA=$(python3 ./build-user-data-bootstrap.py $GITHUB_SHA $PODCAST_RMQ_ADDRESS $BP_MODE)
 
