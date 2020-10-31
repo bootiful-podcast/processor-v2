9e786703815096ebbd3c87a20f3a588efa08820f
Josh Long <josh@joshlong.com>
2020-10-31 04:31:07 -0700

    4gb ought to be enough

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,23 +1,54 @@
 #!/usr/bin/env bash
 
+function read_kubernetes_secret() {
+  kubectl get secret $1 -o jsonpath="{.data.$2}" | base64 --decode
+}
+
 echo "Deploying Processor..."
+BP_MODE=${1:-DEVELOPMENT}
+echo BP_MODE=${BP_MODE}
+if [ "$BP_MODE" = "DEVELOPMENT" ]; then
+  echo "were using the development variables, not the production ones."
+fi
 
-
-APP_NAME=audio-processor
+ROOT_DIR=$(cd $(dirname $0)/.. && pwd)
+echo ROOT_DIR is $ROOT_DIR
+APP_NAME=processor
 PROJECT_ID=${GKE_PROJECT:-pgtm-jlong}
 
-cd $(dirname $0)/..
-root_dir=$(pwd)
+cd $ROOT_DIR
+pwd
 
-pack build -B heroku/buildpacks:18 $APP_NAME
-
-image_id=$(docker images -q $APP_NAME)
-docker tag "${image_id}" gcr.io/${PROJECT_ID}/${APP_NAME}
-
-
-
+#pack build -B heroku/buildpacks:18 $APP_NAME
+#image_id=$(docker images -q $APP_NAME)
+#docker tag "${image_id}" gcr.io/${PROJECT_ID}/${APP_NAME}
 #docker push gcr.io/${PROJECT_ID}/${APP_NAME}
 #docker pull gcr.io/${PROJECT_ID}/${APP_NAME}:latest
+
+API_YAML=${ROOT_DIR}/deploy/processor.yaml
+API_SERVICE_YAML=${ROOT_DIR}/deploy/processor-service.yaml
+SECRETS=${APP_NAME}-secrets
+RMQ_USER=$(read_kubernetes_secret rabbitmq-secrets RABBITMQ_DEFAULT_USER)
+RMQ_PW=$(read_kubernetes_secret rabbitmq-secrets RABBITMQ_DEFAULT_PASS)
+
+kubectl delete secrets ${SECRETS} || echo "could not delete ${SECRETS}."
+kubectl delete -f "$API_YAML" || echo "could not delete the existing Kubernetes environment as described in ${API_YAML}."
+kubectl apply -f <(echo "
+---
+apiVersion: v1
+kind: Secret
+metadata:
+  name: ${SECRETS}
+type: Opaque
+stringData:
+  PODCAST_RMQ_ADDRESS: amqp://${RMQ_USER}:${RMQ_PW}@rabbitmq/
+  AWS_ACCESS_KEY: "${AWS_ACCESS_KEY_ID}"
+  AWS_ACCESS_KEY_SECRET: "${AWS_SECRET_ACCESS_KEY}"
+  AWS_REGION: "${AWS_REGION}"
+")
+
+kubectl apply -f $API_YAML
+kubectl get service | grep $APP_NAME || kubectl apply -f $API_SERVICE_YAML
 #
 #kubectl create deployment ${APP_NAME} --image=gcr.io/${PROJECT_ID}/${APP_NAME}
 #kubectl expose deployment ${APP_NAME} --port=80 --target-port=8080 --name=${APP_NAME} --type=LoadBalancer
